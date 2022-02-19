a44edee5a629006865aa691d0e9263586d89e1b0
parambansreevastav <sreevastav.paramban@kochtc.com>
2022-02-19 10:13:02 +0530

    inital commit

---
+++ olas/README.md
@@ -0,0 +1,2 @@
+# CPP_Package_Managers
+
---
+++ olas/config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Hello World"
+
+cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=/Users/parambansreevastav/Source/vcpkg/scripts/buildsystems/vcpkg.cmake
